3fc2e5ebc05b5455ddce4fceffd93f3528889606
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-11-10 18:12:32 -0500

    Initial code: added views, logo, setup mongodb

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://test123:test123@cluster0.ibkzrfp.mongodb.net/?retryWrites=true&w=majority";
